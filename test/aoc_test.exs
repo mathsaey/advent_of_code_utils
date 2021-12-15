f0cfb702b4aae0752a0d8c52edc4bd06d4b0995d
Mathijs Saey <mathijs@mathsaey.be>
2021-12-15 17:28:12 +0100

    Tests for example_*

--- test/aoc_test.exs
+++ test/aoc_test.exs
@@ -5,6 +5,10 @@ defmodule AOCTest do
     assert AOC.input_path(1991, 8) == "test/input/1991_8.txt"
   end
 
+  test "example_path/2" do
+    assert AOC.example_path(1991, 8) == "test/example/1991_8.txt"
+  end
+
   test "input_string/2" do
     assert AOC.input_string(1991, 8) == """
            first line
@@ -12,36 +16,47 @@ defmodule AOCTest do
            """
   end
 
+  test "example_string/2" do
+    assert AOC.example_string(1991, 8) == """
+           example line
+           other line
+           """
+  end
+
   test "input_stream/2" do
     assert AOC.input_stream(1991, 8) |> Enum.to_list() == ["first line", "second line"]
+  end
+
+  test "example_stream/2" do
+    assert AOC.example_stream(1991, 8) |> Enum.to_list() == ["example line", "other line"]
   end
 
   test "generated functions" do
     defmodule Test do
       use AOC, year: 1991, day: 8
-
-      # input_* functions are private
-      def path, do: input_path()
-      def string, do: input_string()
-      def stream, do: input_stream()
     end
 
-    assert Test.path() == AOC.input_path(1991, 8)
-    assert Test.string() == AOC.input_string(1991, 8)
-    assert Test.stream() == AOC.input_stream(1991, 8)
+    assert Test.input_path() == AOC.input_path(1991, 8)
+    assert Test.input_string() == AOC.input_string(1991, 8)
+    assert Test.input_stream() == AOC.input_stream(1991, 8)
+
+    assert Test.example_path() == AOC.example_path(1991, 8)
+    assert Test.example_string() == AOC.example_string(1991, 8)
+    assert Test.example_stream() == AOC.example_stream(1991, 8)
   end
 
   test "aoc/3" do
     import AOC
 
     aoc 1991, 8 do
-      def path, do: input_path()
-      def string, do: input_string()
-      def stream, do: input_stream()
     end
 
-    assert Y1991.D8.path() == AOC.input_path(1991, 8)
-    assert Y1991.D8.string() == AOC.input_string(1991, 8)
-    assert Y1991.D8.stream() == AOC.input_stream(1991, 8)
+    assert Y1991.D8.input_path() == AOC.input_path(1991, 8)
+    assert Y1991.D8.input_string() == AOC.input_string(1991, 8)
+    assert Y1991.D8.input_stream() == AOC.input_stream(1991, 8)
+
+    assert Y1991.D8.example_path() == AOC.example_path(1991, 8)
+    assert Y1991.D8.example_string() == AOC.example_string(1991, 8)
+    assert Y1991.D8.example_stream() == AOC.example_stream(1991, 8)
   end
 end
