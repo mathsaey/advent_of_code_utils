07c26bf4b5a19b9e8438f2644d612fb436998d38
Mathijs Saey <mathijs@mathsaey.be>
2023-11-18 01:56:44 +0100

    make aoc_test selector more specific

--- .formatter.exs
+++ .formatter.exs
@@ -1,8 +1,8 @@
 # Used by "mix format"
 [
   inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
-  locals_without_parens: [aoc: 3, aoc_test: :*],
+  locals_without_parens: [aoc: 3, aoc_test: 4],
   export: [
-    locals_without_parens: [aoc: 3, aoc_test: :*]
+    locals_without_parens: [aoc: 3, aoc_test: 4]
   ]
 ]
