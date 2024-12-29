7ba36f833913abb57248b700739921d48219d07d
Mathijs Saey <mathijs@mathsaey.be>
2024-12-29 01:40:59 +0100

    Explicit example_string in generated doctest.
    - This gives the user a hint that multiple exmplae_string based tests
      may be provided.

--- lib/mix/tasks/aoc.gen.ex
+++ lib/mix/tasks/aoc.gen.ex
@@ -145,14 +145,14 @@ defmodule Mix.Tasks.Aoc.Gen do
     \"\"\"
 
     @doc \"\"\"
-        iex> p1(example_string())
+        iex> p1(example_string(0))
     \"\"\"
     <% end %>def p1(input) do
       input
     end
 
     <%= if @doctests do %>@doc \"\"\"
-        iex> p2(example_string())
+        iex> p2(example_string(0))
     \"\"\"
     <% end %>def p2(input) do
       input
